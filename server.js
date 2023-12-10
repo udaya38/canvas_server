f93af0fc3604bd6010d1a7969ae760113c82d317
KOSALAI V <kosalaiv@KOSALAIs-MacBook-Pro.local>
2023-12-10 20:08:31 +0530

    final one

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const { MongoClient } = require("mongodb");
 const cors = require("cors");
 const app = express();
 app.use(
@@ -9,27 +11,96 @@ app.use(
   })
 );
 const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  cors: "http://localhost:4200",
-});
+const client = new MongoClient(process.env.MONGO_URL);
+let collection = null;
+async function startUp() {
+  try {
+    await client.connect();
+    const db = client.db("testdb");
+    collection = db.collection("roomDetails");
+    const io = new Server(httpServer, {
+      cors: "http://localhost:4200",
+    });
 
-io.on("connection", (socket) => {
-  console.log(socket.id, "connected");
-  socket.on("beginDraw", (message) => {
-    socket.broadcast.emit("beginDraw", message);
-  });
+    io.on("connection", async (socket) => {
+      //Join the room
+      socket.on("join-room", async (room) => {
+        await collection.updateOne(
+          { roomId: room.roomId },
+          { $addToSet: { users: room.userName } },
+          { upsert: true }
+        );
+        const details = await collection.findOne(
+          { roomId: room.roomId },
+          { projection: { _id: 0 } }
+        );
+        details.userName = room.userName;
+        socket.join(room.roomId);
+        io.to(room.roomId).emit("join-room", details);
+      });
 
-  socket.on("lineDraw", (message) => {
-    socket.broadcast.emit("lineDraw", message);
-  });
+      socket.on("beginDraw", (message, room) => {
+        socket.broadcast.to(room.roomId).emit("beginDraw", message);
+      });
 
-  socket.on("canvasReset", () => {
-    socket.broadcast.emit("canvasReset");
-  });
+      socket.on("lineDraw", (message, room) => {
+        socket.broadcast.to(room.roomId).emit("lineDraw", message);
+      });
 
-  socket.on("configs", (message) => {
-    socket.broadcast.emit("configs", message);
-  });
-});
+      socket.on("canvasReset", (room) => {
+        io.to(room.roomId).to(room.roomId).emit("canvasReset");
+      });
 
-httpServer.listen(3000);
+      socket.on("configs", (message, room) => {
+        socket.broadcast.to(room.roomId).emit("configs", message);
+      });
+
+      //Message Listeners
+      socket.on("word-list", (message, room) => {
+        io.to(room.roomId).emit("word-list", {
+          message,
+          userName: room.userName,
+        });
+      });
+
+      socket.on("user-choosing", (room) => {
+        socket.broadcast
+          .to(room.roomId)
+          .emit(
+            "word-list",
+            { message: "is choosing the word", userName: room.userName },
+            true
+          );
+      });
+
+      socket.on("selected-word", (room) => {
+        socket.broadcast.to(room.roomId).emit("selected-word", room);
+      });
+
+      socket.on("score-details", (room) => {
+        io.to(room.roomId).emit("score-details", room);
+      });
+
+      socket.on("next-user", (room) => {
+        io.to(room.roomId).emit("next-user", room);
+      });
+
+      socket.on("reload", (room) => {
+        io.to(room.roomId).emit("reload", room);
+      });
+
+      socket.on("game-end", (room) => {
+        socket.broadcast.to(room.roomId).emit("game-end", room);
+      });
+    });
+
+    httpServer.listen(3000, () => {
+      console.log("port is listening to 3000");
+    });
+  } catch (error) {
+    console.log(error);
+    client.close();
+  }
+}
+
+startUp();
